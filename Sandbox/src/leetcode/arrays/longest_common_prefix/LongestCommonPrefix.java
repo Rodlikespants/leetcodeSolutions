8c1a367d1a5f4a628835c09fc88c93a78ac42efe
Rod <rodwittlai@gmail.com>
2020-09-02 00:21:23 -0400

    V2 of longest common prefix, vertical scan

--- Sandbox/src/leetcode/arrays/longest_common_prefix/LongestCommonPrefix.java
+++ Sandbox/src/leetcode/arrays/longest_common_prefix/LongestCommonPrefix.java
@@ -1,41 +1,28 @@
 package leetcode.arrays.longest_common_prefix;
 
+// V2, closer to vertical scan
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         // binary search approach
         // first find the shortest length string in the bunch
-        int maxLen = Integer.MAX_VALUE;
-        String shortStr = "";
-        for (int i = 0; i < strs.length; ++i) {
-            if (strs[i].length() < maxLen) {
-                maxLen = strs[i].length();
-                shortStr = strs[i];
-            }
-        }
-
         // use that as the basis for the binary search
-        return search(strs, 0, maxLen, shortStr);
+        if (strs.length == 0) {
+            return "";
+        }
+        return search(strs);
     }
 
     // helper method that will match substring across all strings given a start and end (exclusive)
-    private String search(String[] strs, int start, int end, String shortStr) {
-        if (end == 0) {
-            return "";
-        }
-        if (shortStr.isEmpty()) {
-            return "";
-        }
-
-        String result = "";
-        for (int col = start; col < end; col++) {
+    private String search(String[] strs) {
+        for (int col = 0; col < strs[0].length(); col++) {
+            char ch = strs[0].charAt(col);
             for (int row = 0; row < strs.length; ++row) {
-                if (strs[row].charAt(col) != shortStr.charAt(col)) {
-                    return result;
+                if (col == strs[row].length() || strs[row].charAt(col) != ch) {
+                    return strs[0].substring(0, col);
                 }
             }
-            result += shortStr.charAt(col) + "";
         }
 
-        return result;
+        return strs[0];
     }
 }
